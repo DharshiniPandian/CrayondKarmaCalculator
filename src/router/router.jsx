2dbc70eb51c4f72223f19cf798f9f9a9ce5e13fb
GauthamSrini <gauthamsrinivasan21@gmail.com>
2024-09-27 13:27:43 +0530

    front page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,14 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import Temp from "../pages/Temp";
 import Temp2 from "../pages/Temp2";
 import App from "../App";
+import FrontPage from "../pages/frontpage/FrontPage";
 
 
 function Router() {
   return (
     <Routes>
-   
       <Route element={<Temp />} path="/temp" />
       <Route element={<Temp2 />} path="/temp2" />
+      <Route element={<FrontPage/>} path="/frontpage"/>
     </Routes>
   );
 }
