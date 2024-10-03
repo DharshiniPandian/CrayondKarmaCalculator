ccdd29aa30fecf6feffb528c5ff3cd3418b4b0bc
jayanthan-prog <jayanthan212@gmail.com>
2024-10-03 19:45:03 +0530

    react skeleton update

--- src/pages/Food/Food.jsx
+++ src/pages/Food/Food.jsx
@@ -15,30 +15,43 @@ import { addMasterFoodsData } from "../../slice/MasterApiSlices";
 import { revertTravelDistance, selectFoodType } from "../../slice/CalculationSlice";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import "../../styles/toast.css";
 
 export default function Food() {
-  // const [selectedItem, setSelectedItem] = useState(null);
-  // const [CarbonValue, setCarbonValue] = useState(0);
   const [foodId, setFoodId] = useState(0);
   const [foodValue, setFoodValue] = useState(0);
   const [foodName, setFoodName] = useState(false);
+  const [loading, setLoading] = useState(true); // Loading state for skeleton
   const foodData = useSelector((s) => s.masterFoodItems);
-  const globalCarbonValue = useSelector((s)=>s.carbonValue.total_emission.total_emission)
+  const globalCarbonValue = useSelector((s) => s.carbonValue.total_emission.total_emission);
+  const totalVehicleEmission = useSelector((state) => state.carbonValue.vehicle.total_vehicle_emission);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
   const [value, setValue] = useState(25);
 
-  const totalVehicleEmission = useSelector((state) => state.carbonValue.vehicle.total_vehicle_emission);
+  useEffect(()=>{
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+  },[])
 
   useEffect(() => {
-    let timer;
-    if (value < 50) {
-      timer = setTimeout(() => setValue(value + 1), 20);
+    fetchMasterFoodItems();
+  }, []);
+
+  const fetchMasterFoodItems = async () => {
+    try {
+      const response = await axios.get("http://localhost:8081/master/foods");
+      if (response.status === 200) {
+        dispatch(addMasterFoodsData(response.data));
+        setLoading(true); // Stop loading once data is fetched
+      }
+    } catch (error) {
+      console.log("error while fetching data", error);
     }
-    return () => clearTimeout(timer);
-  }, [value]);
+  };
 
   const styles = [
     {
@@ -62,9 +75,6 @@ export default function Food() {
     setFoodName(food.name);
     setFoodId(food.id);
     setFoodValue(Number(food.value));
-    // console.log("Selected Food Name =", food.name);
-    // console.log("Selected Food ID =", food.id);
-    // console.log("Selected Food Value =", Number(food.value));
   };
 
   const NextFunction = () => {
@@ -73,36 +83,20 @@ export default function Food() {
       navigate("/Appliance", { state: { foodId } });
       dispatch(selectFoodType({ foodId, foodValue }));
     } else {
-      // Display a toast notification if no vehicle is selected
-              toast.warn("Please select a Food Item before proceeding!", {
-                className: "custom-toast", // Custom class for warning toast
-                bodyClassName: "custom-toast-body", // Custom class for the body
-                progressClassName: "custom-toast-progress", // Custom class for the progress bar
-              });
+      toast.warn("Please select a Food Item before proceeding!", {
+        className: "custom-toast",
+        bodyClassName: "custom-toast-body",
+        progressClassName: "custom-toast-progress",
+      });
     }
   };
-
-  useEffect(() => {
-    fetchmasterFoodItems();
-  }, []);
-
-  const fetchmasterFoodItems = async () => {
-    try {
-      const response = await axios.get("http://localhost:8081/master/foods");
-      if (response.status === 200) dispatch(addMasterFoodsData(response.data));
-    } catch (error) {
-      console.log("error while fetching data", error);
-    }
-  };
-
-  // console.log(totalVehicleEmission)
 
   return (
-    <div style={{ width: "100%", border: "1px solid #E8F2FF",height: "100%" }}>
+    <div style={{ width: "100%", border: "1px solid #E8F2FF", height: "100%" }}>
       <div className="food-top">
         <div className="carbon-value">
           <BsTriangleFill style={{ color: "#DF2929", fontWeight: "550" }} />
-          {Number(totalVehicleEmission+foodValue).toFixed(2)} ton CO2
+          {Number(totalVehicleEmission + foodValue).toFixed(2)} ton CO2
         </div>
       </div>
       <div className="food-bottom">
@@ -121,58 +115,65 @@ export default function Food() {
                 backgroundColor: "#F39C12",
               })}
               strokeWidth={12}
-            ></CircularProgressbarWithChildren>
+            />
           </div>
         </div>
         <div className="food-ques">What you normally eat?</div>
         <div className="food-products">
-          {foodData.map((food, key) => (
-            <div
-              key={key}
-              className={`food-items ${
-                foodName === food.name ? "selected" : ""
-              }`}
-              style={{
-                backgroundColor: styles[key].backgroundColor,
-                border: foodName === food.name ? `2px solid ${styles[key].borderColor}` : "0px",
-                //  borderColor : styles[key].transparent,
-                // borderWidth: "2px",
-                // borderStyle: "solid",
-              }}
-              onClick={() => handleItemClicked(food)}
-            >
-              <img
-                src={food.path}
-                style={{ width: "35px", height: "35px" }}
-                alt=""
-              />
-              <div
-                style={{
-                  fontWeight: "420",
-                  fontSize: "13px",
-                  fontFamily: "Excon, sans-serif",
-                }}
-              >
-                {food.name}
-              </div>
-            </div>
-          ))}
-        </div>   
-      <div className="foodStaticBtn" >
-        <Button
-          onBack={() => {
-            dispatch(revertTravelDistance());
-            dispatch(goToPreviousStep());
-            navigate("/Vehicle4", { state: { activepage: "pagefour" } });
-          }}
-          onNext={NextFunction} 
-        />
+          {loading
+            ? Array(3)
+                .fill()
+                .map((_, key) => (
+                  <div key={key} className="skeleton-wrapper">
+                    <Skeleton height={110} width={110} />
+                  </div>
+                ))
+            : foodData.map((food, key) => (
+                <div
+                  key={key}
+                  className={`food-items ${
+                    foodName === food.name ? "selected" : ""
+                  }`}
+                  style={{
+                    backgroundColor: styles[key].backgroundColor,
+                    border:
+                      foodName === food.name
+                        ? `2px solid ${styles[key].borderColor}`
+                        : "0px",
+                  }}
+                  onClick={() => handleItemClicked(food)}
+                >
+                  <img
+                    src={food.path}
+                    style={{ width: "35px", height: "35px" }}
+                    alt=""
+                  />
+                  <div
+                    style={{
+                      fontWeight: "420",
+                      fontSize: "13px",
+                      fontFamily: "Excon, sans-serif",
+                    }}
+                  >
+                    {food.name}
+                  </div>
+                </div>
+              ))}
         </div>
+        <div className="foodStaticBtn">
+          <Button
+            onBack={() => {
+              dispatch(revertTravelDistance());
+              dispatch(goToPreviousStep());
+              navigate("/Vehicle4", { state: { activepage: "pagefour" } });
+            }}
+            onNext={NextFunction}
+          />
         </div>
-      {/* Toast Container for displaying notifications */}
-      <ToastContainer 
+      </div>
+      <ToastContainer
         position="top-center"
-        autoClose={3000} // Set auto-close time
+        autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
@@ -182,6 +183,6 @@ export default function Food() {
         pauseOnHover
         theme="light"
       />
-      </div>
+    </div>
   );
 }
